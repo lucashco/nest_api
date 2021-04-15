df7be72ccebf05da607002200cfc00742bd1cac9
Lucas Castilho <lucas.hco@hotmail.com>
2021-04-15 12:23:07 -0300

    Nest Api

---
+++ src/models/product.model.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+}
